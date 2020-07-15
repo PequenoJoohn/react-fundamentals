2cecd3a43224916991a855d2f15498bcd92aa353
pequenojoohn <kheymk@hotmail.com>
2020-07-15 16:25:07 -0300

    Imutability and add itens in list.

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -3,23 +3,49 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
     state = {
+        newTech: '',
         techs: [
             'Node.JS',
             'ReactJS',
             'React Native'
-        ]
+        ],
+        exist: false
     };
 
-    render() {
-        console.log(this.state);
+    handleInputChange = e => {
+        this.setState({ newTech: e.target.value });
+    }
 
+    handleSubmit = e => {
+        e.preventDefault();
+
+        if(this.state.newTech.length === 0) {
+            this.setState({
+                exist: true,
+            })
+           return console.log("Você não adicionou nenhum campo")
+        }
+        this.setState({
+            techs: [...this.state.techs, this.state.newTech],
+            newTech: '',
+            exist: false,
+        })
+
+        console.log(this.state.newTech);
+    }
+
+    render() {
         return (
-            <ul>
-                {this.state.techs.map((e) => (
-                    <li key={e.id}>{e}</li>
-                ))
-                }
-            </ul>
+            <form onSubmit={this.handleSubmit}>
+                <ul>
+                    {this.state.techs.map(tech => (
+                        <li key={tech}>{tech}</li>
+                    ))}
+                </ul>
+                    { this.state.exist ?  (<p>Os campos não podem ser nulos.</p>) : null }
+                <input type="text" onChange={this.handleInputChange} value={this.state.newTech} />
+                <button type="submit">Enviar</button>
+            </form>
         );
     }
 }
